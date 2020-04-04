8c9b805ac8d067ff71a385a4af6ce13f1dbd04d6
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-04-04 08:13:28 +0530

    push homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:covidapp/UI/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: HomeScreen(),
     );
   }
 }
